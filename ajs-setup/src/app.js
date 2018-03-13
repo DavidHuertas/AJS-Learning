7ae7e200ae82e261f583fda9f89e666b742ca00a
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-03-13 17:59:12 +0100

    AJS-Fundamentals-Filters: Using orderBy and limitTo Filters, with Custom Display Filters

--- ajs-setup/src/app.js
+++ ajs-setup/src/app.js
@@ -4,11 +4,44 @@ angular.module('myApp', []);
 
 angular.module('myApp').controller('MainController', [function() {
 
-	this.user = {
-		name: 'JOHN doe',
+	this.users = [
+	{
+		firstName: 'JOHN',
+		lastName: 'doe',
 		accountType: 'CHECKING',
-		balance: 1349.2
-	};
+		balance: 1349.2,
+		birthday: 25792760000,
+	},
+	{
+		firstName: 'JIMMY ',
+		lastName: 'kiMMEL',
+		accountType: 'CHECKING',
+		balance: 1245.33,
+		birthday: 48826440000,
+	},
+	{
+		firstName: 'DAVid',
+		lastName: 'orChaRDS',
+		accountType: 'CHECKING',
+		balance: 4788.89,
+		birthday: -13580280000,
+	},
+	{
+		firstName: 'LAURA',
+		lastName: 'contoNAHUER',
+		accountType: 'CHECKING',
+		balance: 1349.2,
+		birthday: 24721200000,
+	},
+	{
+		firstName: 'MELISSA ',
+		lastName: 'auf der maur',
+		accountType: 'CHECKING',
+		balance: 1349.2,
+		birthday: 25542760000,
+	}
+
+	];
 
 }]);
 
