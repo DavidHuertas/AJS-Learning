a30ddb0c74e4a13af9f8fcec5989cc2f3f9d334d
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-03-13 18:06:31 +0100

    AJS-Fundamentals-Filters: Using Number and Json Filters

--- ajs-setup/src/app.js
+++ ajs-setup/src/app.js
@@ -4,44 +4,18 @@ angular.module('myApp', []);
 
 angular.module('myApp').controller('MainController', [function() {
 
-	this.users = [
+	this.user = [
 	{
 		firstName: 'JOHN',
 		lastName: 'doe',
-		accountType: 'CHECKING',
+		accountType: {
+			accountId: 1111222233334444,
+			name: 'checking'
+		},
 		balance: 1349.2,
 		birthday: 25792760000,
-	},
-	{
-		firstName: 'JIMMY ',
-		lastName: 'kiMMEL',
-		accountType: 'CHECKING',
-		balance: 1245.33,
-		birthday: 48826440000,
-	},
-	{
-		firstName: 'DAVid',
-		lastName: 'orChaRDS',
-		accountType: 'CHECKING',
-		balance: 4788.89,
-		birthday: -13580280000,
-	},
-	{
-		firstName: 'LAURA',
-		lastName: 'contoNAHUER',
-		accountType: 'CHECKING',
-		balance: 1349.2,
-		birthday: 24721200000,
-	},
-	{
-		firstName: 'MELISSA ',
-		lastName: 'auf der maur',
-		accountType: 'CHECKING',
-		balance: 1349.2,
-		birthday: 25542760000,
-	}
-
-	];
+		hobbies: ['music', 'videogames', 'programming']
+	}];
 
 }]);
 
