378dc0fe91dea879c30975ce9eec550f19b1f2fe
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-03-13 12:39:00 +0100

    AJS-Fundamentals-Introduction: Using Scope Functions

--- ajs-setup/src/app.js
+++ ajs-setup/src/app.js
@@ -4,4 +4,8 @@ angular.module('myApp', []);
 
 angular.module('myApp').controller('MainController', ['$scope', function($scope) {
 	console.log("hello");
+	$scope.message = 'Hello';
+	$scope.sayHello = function(name) {
+		return $scope.message + ' ' + name;
+	};
 }]);
