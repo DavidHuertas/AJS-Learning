0141ab8a7dd045ba2811b2bc3d4e5ae4d2e0a984
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-03-27 12:32:15 +0200

    AJS-Fundamentals-User_Input_And_Validation: Examining User Text Input

--- ajs-setup/src/app.js
+++ ajs-setup/src/app.js
@@ -4,30 +4,25 @@ angular.module('myApp', []);
 
 angular.module('myApp').controller('MainController', [function() {
 
-	this.now = new Date();
+	this.fruitList = ['bananas', 'apples', 'pears', 'cherries', 'peaches'];
 
-	this.user = 
-	{
-		name: 'Dave Grohl',
-		birthday: 171781200000,
-		lastLogin: 1437705300000
-	};
+	this.user = {
+		name: 'John Smith',
+		favouriteFruit: 'cherries',
+		isActive: true
+	}
 
 }]);
 
+angular.module('myApp').filter('yesOrNo', function() {
 
-angular.module('myApp').filter('capitalize', function() {
-	return function(value){
-		var result = null;
-		var words = value.split(' ');
-		words.forEach(function(item) {
-			if (result) {
-				result += ' ';
-			}else{
-				result = '';
-			}
-			result += item.substr(0, 1).toUpperCase() + item.substr(1).toLowerCase();
-		});
-		return result;
+	return function(value) {
+		if (value === true) {
+			return 'yes';
+		} else if (value === false) {
+			return 'no';
+		} else {
+			return 'unknown';
+		}
 	};
 });
