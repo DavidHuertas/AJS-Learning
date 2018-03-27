626ea05fdb38cd0f6efb6696750035f998e2e615
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-03-27 16:46:59 +0200

    AJS-Fundamentals-Working_With_AngularJS: Using Iterators

--- ajs-setup/src/app.js
+++ ajs-setup/src/app.js
@@ -2,75 +2,21 @@
 
 angular.module('myApp', []);
 
-angular.module('myApp').factory('RandomNameService', function($q, $timeout) {
-
-	var firstNames = ['David', 'Scott', 'Joe', 'Michael', 'Mark', 'Scott'];
-	var middleNames = ['Anson', 'Paul', 'George', 'Israel', 'Mathew', 'Todd'];
-	var lastNames = ['Smith', 'Nicolas', 'Milles', 'Roberts', 'Johnson'];
-
-	return {
-
-		getName: function(nameType) {
-
-			var defer = $q.defer();
-			var duration = Math.round(Math.random() * 3000);
-
-			$timeout(function() {
-				var name = '';
-				switch(nameType) {
-					case 'first':
-						name = firstNames[Math.floor(Math.random() * firstNames.length)];
-						break;
-					case 'middle':
-						name = middleNames[Math.floor(Math.random() * middleNames.length)];
-						break;
-					case 'last':
-						name = lastNames[Math.floor(Math.random() * lastNames.length)];
-						break;
-				}
-
-				defer.resolve(name);
-			}, duration);
-
-			return defer.promise;
-		}
-	};
-});
-
-angular.module('myApp').controller('MainController', ['$q', 'RandomNameService', function($q, RandomNameService) {
+angular.module('myApp').controller('MainController', [function() {
 
 	var vm = this;
 
-	var firstNamePromise = RandomNameService.getName('first');
-	var middleNamePromise = RandomNameService.getName('middle');
-	var lastNamePromise = RandomNameService.getName('last');
+	var greetings = ['Hi', 'What\'s up', 'How\'s it going', 'Hello'];
+	var names = ['Anelle', 'Anya', 'Chris', 'Derek', 'Lynn', 'Mike'];
 
-	
-	//EN ESTA PARTE DE CÓDIGO VEMOS CÓMO TARDAN EN CARGAR LOS DISTINTOS HILOS
+	vm.greetingSentences = [];
 
-	firstNamePromise.then(function(name) {
-		vm.firstName = name;
+	angular.forEach(names, function(name) {
+
+		var randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+		var sentence = randomGreeting + ' ' + name;
+
+		vm.greetingSentences.push(sentence);
 	});
-
-	middleNamePromise.then(function(name) {
-		vm.middleName = name;
-	});
-
-	lastNamePromise.then(function(name) {
-		vm.lastName = name;
-	});
-	
-	//_________________________________________
-	//EN ESTA PARTE DE CÓDIGO VEMOS LA MISMA FUNCIÓN ANTERIOR, PERO SÓLO PUBLICA CUANDO 
-	//TODOS LOS HILOS HAN TERMINADO
-	/*
-	var completed = $q.all([firstNamePromise, middleNamePromise, lastNamePromise]);
-
-	completed.then(function(data) {
-		vm.firstName = data[0];
-		vm.middleName = data[1];
-		vm.lastName = data[2];
-	});
-	*/
 
 }]);
