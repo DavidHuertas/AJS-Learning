ed4ba2715700d342699c6cde3292b45b71ba7c01
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-03-27 13:21:27 +0200

    AJS-Fundamentals-Output_Formatting: Filtering Output

--- ajs-setup/src/app.js
+++ ajs-setup/src/app.js
@@ -4,7 +4,23 @@ angular.module('myApp', []);
 
 angular.module('myApp').controller('MainController', [function() {
 
-	this.hello = 'Hello, from AngularJS.';
-	this.name = '';
+	this.items = [
+
+		{name: 'Bananas', type:'fruits'},
+		{name: 'Cherries', type:'fruits'},
+		{name: 'Oranges', type:'fruits'},
+		{name: 'Chicken', type:'meats'},
+		{name: 'Beef', type:'meats'},
+		{name: 'Pork', type:'meats'},
+		{name: 'Cheese', type:'dairy'},
+		{name: 'Yoghurt', type:'dairy'},
+		{name: 'Milk', type:'dairy'},
+		{name: 'Beans', type:'vegetables'},
+		{name: 'Celery', type:'vegetables'},
+		{name: 'Bread', type:'grains'},
+		{name: 'Special case fruits', type:'other'}
+	];
+
+	this.selectedType = '';
 
 }]);
