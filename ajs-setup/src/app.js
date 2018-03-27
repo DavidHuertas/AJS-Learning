b789596d398d6d2d896d90e98a8a68bd3dcb58e6
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-03-27 13:01:11 +0200

    AJS-Fundamentals-Output_Formatting: Using CSS Classes

--- ajs-setup/src/app.js
+++ ajs-setup/src/app.js
@@ -4,16 +4,7 @@ angular.module('myApp', []);
 
 angular.module('myApp').controller('MainController', [function() {
 
-	this.user = {
-
-	};
-
-	this.submitForm = function(form) {
-		if (form.$valid) {
-			window.alert('Passed');
-		} else {
-			window.alert('Failed');
-		}
-	};
+	this.hello = 'Hello, from AngularJS.';
+	this.name = '';
 
 }]);
