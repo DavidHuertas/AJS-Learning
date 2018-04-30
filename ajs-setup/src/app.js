b219bfc31dabd7c4b1bd8f156cabb841bf61158a
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-04-30 14:06:34 +0200

    AJS-Fundamentals-INTRODUCTION_TO_DIRECTIVES-Basic_Directives: Using ng-repeat on Collections

--- ajs-setup/src/app.js
+++ ajs-setup/src/app.js
@@ -4,22 +4,13 @@ angular.module('myApp', []);
 
 angular.module('myApp').controller('MainController', [function() {
 
-	this.user = {
-		name: 'David Orchards',
-		accountType: 'CHECKING',
-		balance: 1349.2,
-		lastLogin: new Date('March 27, 2018 17:12:00')
-	};
+	this.cars = [
+		{make:'Mazda', model:'Miata', year: 2001},
+		{make:'Toyota', model:'Prius', year: 2013},
+		{make:'Tesla', model:'S', year: 2015},
+		{make:'BMW', model:'325i', year: 2012},
+		{make:'Lexus', model:'IS 250', year: 2009},
+		{make:'Subaru', model:'Outback', year: 2014}
+	]
 
 }]);
-
-angular.module('myApp').filter('capitalize', function() {
-
-	return function(value) {
-		var words = value.split(' ');
-		for (var counter = 0; counter < words.length; counter++) {
-			words[counter] = words[counter].substr(0,1).toUpperCase() + words[counter].substr(1).toLowerCase();
-		}
-		return words.join(' ');
-	};
-});
