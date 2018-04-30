2faf9fd465f2b7d487dd346a6ea0ef8500d4c5bf
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-04-30 18:06:45 +0200

    AJS-Fundamentals-INTRODUCTION_TO_DIRECTIVES-Basic_Directives: Controlling Displays

--- app/js/app.js
+++ app/js/app.js
@@ -2,29 +2,16 @@
 
 angular.module('myApp', []);
 
-//THIS IS BAD (1
-var mainCtrl = null;
-
 angular.module('myApp').controller('MainController', [function() {
-
-	//THIS IS BAD (1)
-	mainCtrl = this;
 
 	var vm = this;
 
-	vm.clickCount = 0;
+	vm.showFirstItem = true;
+	vm.showSecondItem = false;
+	vm.hideThirdItem = true;
 
-	vm.updateClickCount = function (amount)  {
-		vm.clickCount += amount;
+	vm.isSecondItemShowing = function () {
+		return vm.showSecondItem;
 	};
 
 }]);
-
-	//THIS IS ALSO BAD (2)
-function externalClick(elem, amount) {
-
-	var $scope = angular.element(elem).scope();
-	mainCtrl.clickCount += parseInt(amount);
-	$scope.$apply();
-
-}
