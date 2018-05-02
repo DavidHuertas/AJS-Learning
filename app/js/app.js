7714386022750413b0d44568657b8d1251bd495b
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-05-02 14:13:02 +0200

    AJS-Fundamentals-INTRODUCTION_TO_DIRECTIVES-Basic_Directives: Using ng-bind, ng-bind-template And ng-non-bindable Directive

--- app/js/app.js
+++ app/js/app.js
@@ -4,16 +4,9 @@ angular.module('myApp', []);
 
 angular.module('myApp').controller('MainController', [function() {
 
-	this.fullName = 'John Smith';
-	this.fullNameConfirm = '';
-	this.message = '';
-
-	this.doChange = function () {
-		if (this.fullName === this.fullNameConfirm) {
-			this.message = 'Thank you for confirming your name.';
-		} else {
-			this.message = '';
-		};
-	};
+	this.standardBind = 'This is a standard data binding.';
+	this.firstTemplateBinding = 'This is the first template binding.';
+	this.secondTemplateBinding = 'this is the second template binding.';
+	this.nonBindable = 'This should not display because we used non-bindable.';
 
 }]);
