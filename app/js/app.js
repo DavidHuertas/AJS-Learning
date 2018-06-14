29872fbcf62e931816f3c7e8de290d39744ff2cc
David Huertas Ruiz <dhuertas.eng@gmail.com>
2018-06-14 14:04:35 +0200

    AJS-Fundamentals-INTRODUCTION_TO_DIRECTIVES-Display_Directives: Using ng-class Directives

--- app/js/app.js
+++ app/js/app.js
@@ -4,21 +4,9 @@ angular.module('myApp', ['ngSanitize']);
 
 angular.module('myApp').controller('MainController', ['$sce', function($sce) {
 
-	this.enableStyles = function() {
-		this.style1 = {
-			color: "red",
-			fontSize: "16px"
-		};
+	this.class1 = 'redClass';
+	this.class2 = 'greenClass';
 
-		this.style2 = {
-			color: "blue",
-			fontSize: "10px"
-		};
-	};
-
-	this.disableStyles = function() {
-		this.style1 = null;
-		this.style2 = null;
-	};
+	this.isSelected = false;
 
 }]);
